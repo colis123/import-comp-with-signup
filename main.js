4fb73e8932af7b99efc00623525ca101d1d1c442
Pedro Colis <colis321@yahoo.com>
2019-06-11 23:03:44 -0700

    lastname validation

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-document.querySelector('#firstName').addEventListener('blur', validateName);
+document.querySelector('#firstName').addEventListener('focusout', validateName);
+document.querySelector('#lastName').addEventListener('focusout', validateLastname);
+document.querySelector('#email').addEventListener('focusout', validateEmail);
+document.querySelector('#password').addEventListener('focusout', validatePassword);
 
-const invalid =  document.querySelector('.invalid');
 
 function validateName () {
   const name = document.querySelector('#firstName');
   const re = /^[a-zA-Z]{2,10}$/;
 
-
   if(!re.test(name.value)) {
     document.querySelector('.invalid').style.display = 'block';
   } else {
     document.querySelector('.invalid').style.display = 'none';
+  }
+}
+
+function validateLastname() {
+  const name = document.querySelector('#lastName');
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if(!re.test(name.value)) {
+    document.querySelector('.invalidL').style.display = 'block';
+  } else {
+    document.querySelector('.invalidL').style.display = 'none';
   }
 }
